cc8fad0c8ab33307e5e178ce0ea103620b63e2c2
Raghu Ram <35891786+R4RPA@users.noreply.github.com>
2024-08-24 00:21:27 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,15 +1,1 @@
-pip install cryptography
-
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-
-# Read the .crt file
-with open("yourfile.crt", "rb") as crt_file:
-    crt_data = crt_file.read()
-
-# Load the certificate
-cert = x509.load_pem_x509_certificate(crt_data, default_backend())
-
-# Write the certificate to a .pem file
-with open("yourfile.pem", "wb") as pem_file:
-    pem_file.write(cert.public_bytes(encoding=x509.Encoding.PEM))
+cmd /c "pushd C:\MyFolder && fastboot_relayplus_loader.bat -p bootloader -a && popd"
