ee1b077a601f1460c1ea3ed1f3ab442ea801538d
Raghu Ram <35891786+R4RPA@users.noreply.github.com>
2024-12-20 01:26:32 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,1 +1,4 @@
-cmd /c "pushd C:\MyFolder && fastboot_relayplus_loader.bat -p bootloader -a && popd"
+adb shell am broadcast -a com.android.settings.wifi.action.ADD_NETWORK --es ssid "WiFi_SSID" --es password "WiFi_Password"
+adb shell svc wifi enable
+adb shell svc wifi connect "WiFi_SSID" --password "WiFi_Password"
+adb shell dumpsys wifi
