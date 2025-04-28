88d83c54c09b26cf289716e6f6632e3c4cbc86a0
Raghu Ram <35891786+R4RPA@users.noreply.github.com>
2025-04-29 00:40:55 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,4 +1,23 @@
-adb shell am broadcast -a com.android.settings.wifi.action.ADD_NETWORK --es ssid "WiFi_SSID" --es password "WiFi_Password"
-adb shell svc wifi enable
-adb shell svc wifi connect "WiFi_SSID" --password "WiFi_Password"
-adb shell dumpsys wifi
+@echo off
+setlocal EnableDelayedExpansion
+
+::--- helper -------------------------------------------------
+::   1st arg = folder path where script is located
+::   2nd arg = script name
+:RunPy
+  pushd "%~1"
+  echo(
+  echo ==== Running %~2 in %CD% ====
+  rem run script in new window, wait for it to complete, then exit
+  start "py:%~2" /wait cmd /c "python %~2"
+  popd
+  goto :eof
+::------------------------------------------------------------
+
+call :RunPy "C:\Path\to\project"  my_code1.py
+call :RunPy "C:\Path\to\project"  my_code2.py
+call :RunPy "C:\Path\to\project"  my_code3.py
+
+echo(
+echo All scripts finished.
+pause
